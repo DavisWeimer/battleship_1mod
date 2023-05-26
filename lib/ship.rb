1b5d8d9fc86884587d55d1c1453d76cda19b9451
Wil Fady <wrfady@gmail.com>
2023-05-25 22:39:43 -0400

    feat: Create Ship class

---
+++ lib/ship.rb
@@ -0,0 +1,16 @@
+class Ship
+  attr_reader :name, :length
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @sunk = false
+  end
+
+  def health
+    @length
+  end
+
+  def sunk?
+    @sunk
+  end
+end
