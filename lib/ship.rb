3502f8757fbea948f53849b1e161b2087e45cdc5
Wil Fady <wrfady@gmail.com>
2023-05-26 17:51:45 -0400

    feat: Create sunk and hit methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,16 +1,16 @@
 class Ship
-  attr_reader :name, :length
+  attr_reader :name, :length, :health
   def initialize(name, length)
     @name = name
     @length = length
-    @sunk = false
-  end
-
-  def health
-    @length
+    @health = length
   end
 
   def sunk?
-    @sunk
+    @health == 0
+  end
+
+  def hit
+    @health -= 1
   end
 end
