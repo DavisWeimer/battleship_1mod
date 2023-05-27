417e5adf23a366cf7ca3300b153285022419e280
Wil Fady <wrfady@gmail.com>
2023-05-27 17:35:20 -0400

    feat: Complete render tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe Cell do
 # ——————————— RENDER ——————————
 
   describe "Class" do
-    xit 'can render .' do
+    it 'can render . and M' do
       cell_1 = Cell.new("B4")
       expect(cell_1.render).to eq(".")
       cell_1.fire_upon
@@ -51,7 +51,31 @@ RSpec.describe Cell do
       cell_2.place_ship(cruiser)
       cell_2.render
       expect(cell_2.ship).to eq(cruiser)
-      expect(cell_1.render).to eq(".")
+      expect(cell_2.render).to eq(".")
+    end
+
+    it 'can render S' do
+      cell_2 = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cell_2.render).to eq(".")
+      expect(cell_2.render(true)).to eq(".")
+      cell_2.place_ship(cruiser)
+      expect(cell_2.render(true)).to eq("S")
+    end
+
+    it 'can render H when ship hit and X when ship sunk' do
+      cell_2 = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+      cell_2.place_ship(cruiser)
+      expect(cell_2.render).to eq(".")
+      cell_2.fire_upon
+      expect(cell_2.render).to eq("H")
+      expect(cruiser.sunk?).to eq(false)
+      cruiser.hit
+      expect(cruiser.health).to eq(1)
+      cruiser.hit
+      expect(cruiser.sunk?).to eq(true)
+      expect(cell_2.render).to eq("X")
     end
   end
 end
