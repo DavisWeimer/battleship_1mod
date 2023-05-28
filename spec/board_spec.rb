1429b94dc47569f9893f872b378ee689a6e8c9f0
Davis Weimer <davisweimer@gmail.com>
2023-05-28 12:35:23 -0600

    feat: Create test for valid_placement? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,8 @@
 require './lib/board'
+require './lib/ship'
 
 RSpec.describe Board do
-  describe Board do
+  describe 'class' do
     it 'exists' do
       board = Board.new
       expect(board).to be_a(Board)
@@ -14,13 +15,21 @@ RSpec.describe Board do
     end
   end
   describe 'coordinates' do
-    it ' can validate coordinates' do
+    it 'can validate passed in coordinate' do
       board = Board.new
       expect(board.valid_coordinate?("A1")).to eq true
       expect(board.valid_coordinate?("D4")).to eq true
       expect(board.valid_coordinate?("A5")).to eq false
       expect(board.valid_coordinate?("E1")).to eq false
       expect(board.valid_coordinate?("A22")).to eq false
+    end
+
+    it 'can validate ship placement only equal to length' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq false
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq false
     end
   end
 end
