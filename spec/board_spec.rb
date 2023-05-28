e39b8f6759021cf3ccfa4da418cf6d6190b56c34
Davis Weimer <davisweimer@gmail.com>
2023-05-28 12:16:11 -0600

    feat: Create passing test for valid_coordinate? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,5 +13,14 @@ RSpec.describe Board do
       expect(board.cells.count).to eq(16)
     end
   end
-  
+  describe 'coordinates' do
+    it ' can validate coordinates' do
+      board = Board.new
+      expect(board.valid_coordinate?("A1")).to eq true
+      expect(board.valid_coordinate?("D4")).to eq true
+      expect(board.valid_coordinate?("A5")).to eq false
+      expect(board.valid_coordinate?("E1")).to eq false
+      expect(board.valid_coordinate?("A22")).to eq false
+    end
+  end
 end
