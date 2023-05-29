b95bfd146d7472fad5c38600c56e17f88d41210b
Davis Weimer <davisweimer@gmail.com>
2023-05-29 16:49:39 -0600

    feat: Create passing test for overlapped ships

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -79,5 +79,15 @@ RSpec.describe Board do
       expect(cell_1.ship == cell_2.ship).to eq true
       expect(cell_3.ship == cell_2.ship).to eq true
     end
+
+    it 'cannot validate overlapped ships' do 
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+
+      board.place(cruiser, ["A1", "A2", "A3"])
+
+      submarine = Ship.new("Submarine", 2)
+      expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq false
+    end
   end
 end
