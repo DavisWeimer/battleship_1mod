685993917a7b6d708f3fd6ddad76a018804e3f6c
Davis Weimer <davisweimer@gmail.com>
2023-05-31 08:55:42 -0600

    methodable: Move coordinate validators and randomizer methods from Game class

---
+++ lib/methodable.rb
@@ -0,0 +1,29 @@
+module Methodable
+  
+  def valid_horiz_coords(ship)
+    key_coord = @npc_board.current_board.keys
+
+    unvalidated_hoz_coords = []
+    key_coord.each_cons(ship.length) {|coord| unvalidated_hoz_coords << coord}
+    
+    validated_hoz = unvalidated_hoz_coords.select do |coord_array|
+      @npc_board.valid_placement?(ship, coord_array)
+    end
+  end
+
+  def valid_vert_coords(ship)
+    key_coord = @npc_board.current_board.keys
+    vertical_coord = key_coord.sort_by.with_index {|element, index| [index % 4, element]}
+
+    unvalidated_vert_coords = []
+    vertical_coord.each_cons(ship.length) {|coord| unvalidated_vert_coords << coord}
+
+    validated_vert = unvalidated_vert_coords.select do |coord_array|
+      @npc_board.valid_placement?(ship, coord_array)
+    end 
+  end
+
+  def coord_randomizer(ship)
+    valid_horiz_coords(ship).concat(valid_vert_coords(ship)).sample
+  end
+end
